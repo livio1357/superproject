09cd97c1208acfa42ab50fffefeaba25c560c466
livio bolzon <livio@actingstudiochicago.com>
2017-10-16 19:17:16 -0500

    commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,39 @@
+var quizQuestions = [
+  {
+      question: "What is 5 + 5 = ",
+      answers: [
+          {
+              type: "Math",
+              content: "10"
+          },
+          {
+              type: "Name",
+              content: "ben"
+          },
+          {
+              type: "Animail",
+              content: "dog"
+          }
+      ]
+  },
+  {
+      question: "Which answer would best describe a dog",
+      answers: [
+          {
+              type: "Math",
+              content: "55"
+          },
+          {
+              type: "Name",
+              content: "Jake"
+          },
+          {
+              type: "Animail",
+              content: "Animail"
+          }
+      ]
+  }
+  
+];
+
+export default quizQuestions;
